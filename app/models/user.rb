a1b68a40e922736a0c520907b2baba85296e94f4
Santiago Guerra <guerrasantiago19@gmail.com>
2020-08-23 22:11:10 -0500

    Create and Test  User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+  validates :username, uniqueness: true
+  validates :password_digest, presence: true
+
+  has_secure_password
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    first_name { Faker::Name.name}
+    last_name { Faker::Name.name}
+    username { Faker::Name.unique.name}
+    password 'foobar'
+  end
+end
